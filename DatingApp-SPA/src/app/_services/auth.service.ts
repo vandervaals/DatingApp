5ffedb5b6aa0df7a53632430e4c7d5ee4d427971
vandervaals <domaninvlad95@gmail.com>
2020-01-07 01:08:59 +0300

    End of section 9

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -2,33 +2,34 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  baseUrl = 'http://localhost:5000/api/auth/';
+  baseUrl = environment.apiUrl + 'auth/';
   jwtHelper = new JwtHelperService();
   decodeToken: any;
 
-constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-login(model: any) {
-  return this.http.post(this.baseUrl + 'login', model)
-  .pipe(
-    map((response: any) => {
-      const user = response;
-      if (user) {
-        localStorage.setItem('token', user.token);
-        this.decodeToken = this.jwtHelper.decodeToken(user.token);
-      }
-    })
-  );
-}
+  login(model: any) {
+    return this.http.post(this.baseUrl + 'login', model)
+    .pipe(
+      map((response: any) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('token', user.token);
+          this.decodeToken = this.jwtHelper.decodeToken(user.token);
+        }
+      })
+    );
+  }
 
-register(model: any) {
-  return this.http.post(this.baseUrl + 'register', model);
-}
+  register(model: any) {
+    return this.http.post(this.baseUrl + 'register', model);
+  }
 
   loggedIn() {
     const token = localStorage.getItem('token');
